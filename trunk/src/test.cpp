861d0d1a43b6f04e2ffb9411ad82524fdefc61e5
Eliezer Talón <elitalon@gmail.com>
2009-04-17 11:05:08 +0000

    - Mejorado el programa de prueba y añadidas nuevas opciones

--- trunk/src/test.cpp
+++ trunk/src/test.cpp
@@ -26,19 +26,28 @@ int main (int argc, char *argv[])
 {
 	try
 	{
-		// Declare program supported options.
-		po::options_description availableOptions("Available options");
-		availableOptions.add_options()
-			("help,h",		"Print this help message")
-			("file,f",			po::value<std::string>(), "Use the content of a file as reference text to execute a dataset training instead of an OCR. Superseded by the <result> option.")
-			("result,r",		po::value<std::string>(), "Use a string as reference text to execute a dataset training instead of an OCR.")
-			("text,t",			po::value<std::string>(), "Use a plain text file as dataset.")
-			("database,d",		po::value<std::string>(), "Use a database as dataset (e.g. db_nessie). Superseded by the <filename> option.")
-			("username,u",		po::value<std::string>(), "Username when connecting to the database. The <database> argument will be used by default.")
-			("password,p",		po::value<std::string>(), "Password when connecting to the database. The <user> argument will be used by default.")
-			("image",			po::value<std::string>(), "Input image file to process");
+		// Declare program arguments and options
+		std::string inputImage;
+
+		po::options_description visibleOptions("Options");
+		visibleOptions.add_options()
+			("file,f",				po::value<std::string>(), "Use a plain text file as classification dataset.")
+			("database,d",			po::value<std::string>()->default_value("db_nessieocr"), "Use a database as classification dataset. Superseded by the --file option.")
+			("user,u",				po::value<std::string>()->default_value("nessieocr"), "Database user.")
+			("password,p",			po::value<std::string>()->default_value("nessieocr"), "Database user's password.")
+			("training,t",			po::value<std::string>(), "Use a plain text file as reference text to execute a training stage.")
+			("create-patterns,c",	"Create an output BMP image for each pattern found in the input image.")
+			("statistics,s",		"Show statistical data regarding the OCR process.")
+			("help,h",				"Print this help message");
+		po::options_description hiddenOptions("Hidden options");
+		
+		hiddenOptions.add_options() ("image", po::value<std::string>(&inputImage), "Input image file to process");
 		po::positional_options_description p;
 		p.add("image", -1);
+
+		po::options_description availableOptions("Available options");
+		availableOptions.add(visibleOptions).add(hiddenOptions);
+
 
 		// Parse the command line
 		po::variables_map passedOptions;
@@ -46,105 +55,70 @@ int main (int argc, char *argv[])
 		po::notify(passedOptions);
 
 
-		// Help option
+		// Show help message
 		if ( passedOptions.count("help") )
 		{
-			std::cout << "Usage: tester [options] <image>" << std::endl;
-			std::cout << availableOptions;
+			std::cout << std::endl << "Usage: tester [options] <image>" << std::endl;
+			std::cout << visibleOptions << std::endl;
 			return 0;
 		}
 
 
-		// The image filename is mandatory.
+		// Load input image
 		if ( !passedOptions.count("image") )
 		{
-			std::cerr << "The image file was not specified." << std::endl;
+			std::cerr << "tester: Missing image file." << std::endl;
+			std::cerr << std::endl << "Usage: tester [options] <image>" << std::endl;
+			std::cerr << visibleOptions << std::endl;
 			return 1;
 		}
-		Magick::Image image;
-		//image.resolutionUnits(Magick::PixelsPerInchResolution);
-		//image.density("800x800");
-		image.read( passedOptions["image"].as<std::string>() );
+		Magick::Image image( inputImage );
+		if ( image.magick() == "PDF" )
+		{
+			image.resolutionUnits(Magick::PixelsPerInchResolution);
+			image.density("800x800");
+			image.read( inputImage );
+		}
 		Clip pressClip(image, 0, 0, image.rows(), image.columns());
 	
 
-		// Dataset option
-		if ( !passedOptions.count("database") && !passedOptions.count("text") )
-		{
-			std::cerr << "The classification dataset was not specified." << std::endl;
-			return 1;
-		}
-
+		// Load dataset
 		std::auto_ptr<Dataset> dataset;
-		if ( passedOptions.count("text") )
-		{
-			if ( passedOptions["text"].as<std::string>() != "" )
-			{
-				std::string filename( passedOptions["text"].as<std::string>() );
-				dataset.reset( new PlainTextDataset(filename) );
-			}
-			else
-			{
-				std::cerr << "The plain text file was not correctly specified." << std::endl;
-				return 1;
-			}
-		}
+		if ( passedOptions.count("file") )
+			dataset.reset( new PlainTextDataset(passedOptions["file"].as<std::string>()) );
 		else
 		{
-			if ( passedOptions["database"].as<std::string>() != "" )
-			{
-				std::string dbName( passedOptions["database"].as<std::string>() );
-				std::string username(dbName);
-				std::string password(username);
-
-				if ( passedOptions.count("username") )
-				{
-					username = passedOptions["username"].as<std::string>();
-					password = username;
-				}
-
-				if ( passedOptions.count("password") )
-					password = passedOptions["password"].as<std::string>();
-
-				dataset.reset( new PostgreSqlDataset(dbName, username, password) );
-				//dataset.reset( new MySqlDataset(dbName, username, password) );
-			}
-			else
-			{
-				std::cerr << "The database name was not correctly specified." << std::endl;
-				return 1;
-			}
+			std::string databaseName( passedOptions["database"].as<std::string>() );
+			std::string databaseUser( passedOptions["user"].as<std::string>() );
+			std::string databasePassword( passedOptions["password"].as<std::string>() );
+			
+			dataset.reset( new PostgreSqlDataset(databaseName, databaseUser, databasePassword) );
 		}
 		Recognizer recon( dataset );
 
 	
-		// Training options
-		if ( passedOptions.count("file") || passedOptions.count("result") )
+		// Load reference text for training
+		if ( passedOptions.count("training") )
 		{
-			if ( passedOptions.count("result") )
-				recon.trainClassifier(pressClip, passedOptions["result"].as<std::string>(), ClassificationParadigm::knn());
-			else
+			std::ifstream inputFile( passedOptions["training"].as<std::string>().data() );
+			if ( not inputFile.is_open() || not inputFile.good() )
 			{
-				std::ifstream inputFile( passedOptions["file"].as<std::string>().data() );
-				if ( not inputFile.is_open() or not inputFile.good() )
-				{
-					std::cerr << "The file passed for training is not valid." << std::endl;
-					return 1;
-				}
-
-				std::string text("");
-				while ( inputFile.good() )
-				{   
-					std::string line;
-					getline(inputFile, line);
-
-					if (!line.empty())
-						text.append(line);
-				}   
-				inputFile.close();
-
-				recon.trainClassifier(pressClip, text, ClassificationParadigm::knn());
+				std::cerr << "tester: The file passed for training is not valid." << std::endl;
+				return 1;
 			}
+
+			std::string text;
+			while ( inputFile.good() )
+			{   
+				std::string line;
+				getline(inputFile, line);
+
+				if (!line.empty())
+					text.append(line);
+			}   
+			inputFile.close();
+
+			recon.trainClassifier(pressClip, text, ClassificationParadigm::knn());
 		}
 		else
 		{
@@ -152,18 +126,26 @@ int main (int argc, char *argv[])
 			std::cout << "Text extracted    : " << recon.text().content() << std::endl;
 		}
 
-		unsigned int patternNo = 0;
-		std::vector<Pattern> patterns = recon.patterns();
-		for ( std::vector<Pattern>::iterator i = patterns.begin(); i != patterns.end(); ++i )
+
+		// Create BMP images for patterns
+		if ( passedOptions.count("create-patterns") )
 		{
-			std::ostringstream ostr;
-			ostr << patternNo++;
-			std::string filename("pattern");
-			filename.append(ostr.str().append(".bmp"));
-			i->writeToOutputImage(filename,true);
+			unsigned int patternNo = 0;
+			std::vector<Pattern> patterns = recon.patterns();
+			for ( std::vector<Pattern>::iterator i = patterns.begin(); i != patterns.end(); ++i )
+			{
+				std::ostringstream ostr;
+				ostr << patternNo++;
+				std::string filename("pattern");
+				filename.append(ostr.str().append(".bmp"));
+				i->writeToOutputImage(filename,true);
+			}
 		}
 		
-		recon.printStatistics();
+
+		// Show statistics
+		if ( passedOptions.count("statistics") )
+			recon.printStatistics();
 	}
 	catch (std::exception &e)
 	{
