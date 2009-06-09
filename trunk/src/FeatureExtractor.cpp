43166acbd89fb4deea57812016e9747c4c675a25
Eliezer Talón <elitalon@gmail.com>
2009-06-09 03:40:47 +0000

    - Solucionado un bug en la implementación del método FeatureExtractor::computeMoments() que no tomaba números reales para calcular las potencias negativas.

--- trunk/src/FeatureExtractor.cpp
+++ trunk/src/FeatureExtractor.cpp
@@ -25,39 +25,54 @@ void FeatureExtractor::computeMoments (const std::vector<Pattern>& patterns)
 		FeatureVector fv(14);	// Initial value set to zero
 
 		std::pair<unsigned int, unsigned int> centroid = i->centroid();
+		double xc = centroid.first;
+		double yc = centroid.second;
 
-		fv.at(0) = centroid.first;
-		fv.at(1) = centroid.second;
+		fv.at(0) = xc;
+		fv.at(1) = yc;
 		
 		for ( unsigned int j = 0; j < i->height(); ++j )
 		{
-			double tj2 = pow(j-centroid.first, 1);	// p = 1
-			double tj3 = pow(j-centroid.first, 2);	// p = 2
-			double tj4 = pow(j-centroid.first, 0);	// p = 0
-			double tj5 = pow(j-centroid.first, 2);	// p = 2
-			double tj6 = pow(j-centroid.first, 1);	// p = 1
-			double tj7 = pow(j-centroid.first, 2);	// p = 2
-			double tj8 = pow(j-centroid.first, 3);	// p = 3
-			double tj9 = pow(j-centroid.first, 0);	// p = 0
-			double tj10 = pow(j-centroid.first, 3);	// p = 3
-			double tj11 = pow(j-centroid.first, 1);	// p = 1
-			double tj12 = pow(j-centroid.first, 3);	// p = 3
-			double tj13 = pow(j-centroid.first, 2);	// p = 2
+			double tj2 = pow(j-xc, 1);	// p = 1
+			double tj3 = pow(j-xc, 2);	// p = 2
+			double tj4 = pow(j-xc, 0);	// p = 0
+			double tj5 = pow(j-xc, 2);	// p = 2
+			double tj6 = pow(j-xc, 1);	// p = 1
+			double tj7 = pow(j-xc, 2);	// p = 2
+			double tj8 = pow(j-xc, 3);	// p = 3
+			double tj9 = pow(j-xc, 0);	// p = 0
+			double tj10 = pow(j-xc, 3);	// p = 3
+			double tj11 = pow(j-xc, 1);	// p = 1
+			double tj12 = pow(j-xc, 3);	// p = 3
+			double tj13 = pow(j-xc, 2);	// p = 2
 
 			for (unsigned int k = 0; k < i->width(); ++k )
 			{
-				fv.at(2)	+= tj2	* pow(k-centroid.second, 1) * i->at(j,k);	// q = 1
-				fv.at(3)	+= tj3	* pow(k-centroid.second, 0) * i->at(j,k);	// q = 0
-				fv.at(4)	+= tj4	* pow(k-centroid.second, 2) * i->at(j,k);	// q = 2
-				fv.at(5)	+= tj5	* pow(k-centroid.second, 1) * i->at(j,k);	// q = 1
-				fv.at(6)	+= tj6	* pow(k-centroid.second, 2) * i->at(j,k);	// q = 2
-				fv.at(7)	+= tj7	* pow(k-centroid.second, 2) * i->at(j,k);	// q = 2
-				fv.at(8)	+= tj8	* pow(k-centroid.second, 0) * i->at(j,k);	// q = 0
-				fv.at(9)	+= tj9	* pow(k-centroid.second, 3) * i->at(j,k);	// q = 3
-				fv.at(10)	+= tj10	* pow(k-centroid.second, 1) * i->at(j,k);	// q = 1
-				fv.at(11)	+= tj11	* pow(k-centroid.second, 3) * i->at(j,k);	// q = 3
-				fv.at(12)	+= tj12	* pow(k-centroid.second, 2) * i->at(j,k);	// q = 2
-				fv.at(13)	+= tj13	* pow(k-centroid.second, 3) * i->at(j,k);	// q = 3
+				double tk2 = pow(k-yc, 1);	// q = 1
+				double tk3 = pow(k-yc, 0);	// q = 0
+				double tk4 = pow(k-yc, 2);	// q = 2
+				double tk5 = pow(k-yc, 1);	// q = 1
+				double tk6 = pow(k-yc, 2);	// q = 2
+				double tk7 = pow(k-yc, 2);	// q = 2
+				double tk8 = pow(k-yc, 0);	// q = 0
+				double tk9 = pow(k-yc, 3);	// q = 3
+				double tk10 = pow(k-yc, 1);	// q = 1
+				double tk11 = pow(k-yc, 3);	// q = 3
+				double tk12 = pow(k-yc, 2);	// q = 2
+				double tk13 = pow(k-yc, 3);	// q = 3
+
+				fv.at(2)	+= tj2	* tk2	* i->at(j,k);
+				fv.at(3)	+= tj3	* tk3	* i->at(j,k);
+				fv.at(4)	+= tj4	* tk4	* i->at(j,k);
+				fv.at(5)	+= tj5	* tk5	* i->at(j,k);
+				fv.at(6)	+= tj6	* tk6	* i->at(j,k);
+				fv.at(7)	+= tj7	* tk7	* i->at(j,k);
+				fv.at(8)	+= tj8	* tk8	* i->at(j,k);
+				fv.at(9)	+= tj9	* tk9	* i->at(j,k);
+				fv.at(10)	+= tj10	* tk10	* i->at(j,k);
+				fv.at(11)	+= tj11	* tk11	* i->at(j,k);
+				fv.at(12)	+= tj12	* tk12	* i->at(j,k);
+				fv.at(13)	+= tj13	* tk13	* i->at(j,k);
 			}
 		}
 
